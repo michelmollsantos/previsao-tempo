d74ff056160beab92b35ee6d6b6cf73e9d40b2df
Michel Moll Santos <michelmollsantos@gmail.com>
2023-03-16 11:55:35 -0300

    Reformulação do app que mostra o clima hoje

--- js/weather.js
+++ js/weather.js
@@ -1,26 +1,106 @@
-"use strict"
+//variaveis e seleção de elementos
+const apiKey = "306a7b2b4d1ed5a14ebb419a42d705a4"
+const apiCountryURL = "https://www.countryflagicons.com/SHINY/64/"
 
+const apiUnsplash = "https://source.unsplash.com/1600x900/?";
 
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
 
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Sao+Paulo&appid=306a7b2b4d1ed5a14ebb419a42d705a4&lang=pt_br";
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
 
-$.ajax({
-    url: url,
-    success: function (result) {
-        console.log(result);
-        console.log(result.name);
+const weatherContainer = document.querySelector("#weather-data");
 
-        $("#location").text(result.name);
+const errorMessageContainer = document.querySelector("#error-message");
+const loader = document.querySelector("#loader");
 
+const suggestionContainer = document.querySelector("#suggestions");
+const suggestionButtons = document.querySelectorAll("#suggestions button");
 
-        let c = Math.round(result.main.temp - 273.15);
-        let celsius = c.toString();
-        $("#temperature").text(celsius);
+// Loader
+const toggleLoader = () => {
+  loader.classList.toggle("hide");
+};
 
+const getWeatherData = async (city) => {
+  toggleLoader();
 
-        $("#wind").text(result.wind.speed);
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
 
+  const res = await fetch(apiWeatherURL);
+  const data = await res.json();
 
-        $("#sky").text(result.weather[0].description)
-    }
-})
+  toggleLoader();
+
+  return data;
+};
+
+// Tratamento de erro
+const showErrorMessage = () => {
+  errorMessageContainer.classList.remove("hide");
+};
+
+const hideInformation = () => {
+  errorMessageContainer.classList.add("hide");
+  weatherContainer.classList.add("hide");
+
+  suggestionContainer.classList.add("hide");
+};
+
+const showWeatherData = async (city) => {
+  hideInformation();
+
+  const data = await getWeatherData(city);
+
+  if (data.cod === "404") {
+    showErrorMessage();
+    return;
+  }
+
+  cityElement.innerText = data.name;
+  tempElement.innerText = parseInt(data.main.temp);
+  descElement.innerText = data.weather[0].description;
+  weatherIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+  );
+  countryElement.setAttribute("src", apiCountryURL + data.sys.country+".png");
+  umidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed}km/h`;
+
+  // Change bg image
+  document.body.style.backgroundImage = `url("${apiUnsplash + city}")`;
+
+  weatherContainer.classList.remove("hide");
+};
+
+searchBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  const city = cityInput.value;
+
+  showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e) => {
+  if (e.code === "Enter") {
+    const city = e.target.value;
+
+    showWeatherData(city);
+  }
+});
+
+// Sugestões
+suggestionButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const city = btn.getAttribute("id");
+
+    showWeatherData(city);
+  });
+});
